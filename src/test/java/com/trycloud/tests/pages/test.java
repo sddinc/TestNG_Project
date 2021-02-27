9e56e3ffd98fa5392009a5050d51122136e8adab
seljukqa <seljukqa@gmail.com>
2021-02-27 14:21:38 -0500

    sinan

--- src/test/java/com/trycloud/tests/pages/test.java
+++ src/test/java/com/trycloud/tests/pages/test.java
@@ -7,5 +7,6 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println(ConfigurationReader.getProperty("selcuk"));
+        System.out.println("Sinan bir serfsiz....");
     }
 }
