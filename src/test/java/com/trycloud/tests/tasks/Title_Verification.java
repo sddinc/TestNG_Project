978639e141df2af4fcb080843c76786b5fca9af9
seljukqa <seljukqa@gmail.com>
2021-02-27 18:28:39 -0500

    Complete aoutomation of google title and cybertek url

--- src/test/java/com/trycloud/tests/tasks/Title_Verification.java
+++ src/test/java/com/trycloud/tests/tasks/Title_Verification.java
@@ -1,17 +1,22 @@
 package com.trycloud.tests.tasks;
 
 import com.sun.org.glassfish.gmbal.Description;
+import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Title_Verification {
 
 
     @Test(description = "TC #1: Google Title Verification")
-
     public void googleTitleVerification() {
-        Driver.getDriver().get("urlGoogle");
-
+        Driver.getDriver().get(ConfigurationReader.getProperty("urlGoogle"));
+        String expectedTitle = "Google";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle, "Wrong title");
     }
+
+
 }
