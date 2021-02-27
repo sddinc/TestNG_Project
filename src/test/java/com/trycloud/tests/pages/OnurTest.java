88d76bf30cb5c7f4c5c4f35386198f1fe00a1b28
ocelik66il08e <usaonurcelik@gmail.com>
2021-02-27 15:42:41 -0500

    New codes

---
+++ src/test/java/com/trycloud/tests/pages/OnurTest.java
@@ -0,0 +1,10 @@
+package com.trycloud.tests.pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OnurTest {
+
+    WebDriver driver = new ChromeDriver();
+}
