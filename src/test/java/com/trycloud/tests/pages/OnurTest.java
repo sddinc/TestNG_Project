241df3c6f2832d0fc816a2430bac14d5cd31509c
ocelik66il08e <usaonurcelik@gmail.com>
2021-02-28 14:22:33 -0500

    OnurTest

---
+++ src/test/java/com/trycloud/tests/pages/OnurTest.java
@@ -0,0 +1,8 @@
+package com.trycloud.tests.pages;
+
+public class OnurTest {
+    public static void main(String[] args) {
+        System.out.println("selcuk.........");
+        System.out.println("selcuk.........");
+    }
+}
