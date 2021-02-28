2e55a8db0c0cbb1205e82e292638649b76195f64
seljukqa <seljukqa@gmail.com>
2021-02-28 00:33:45 -0500

    Stored google serach WebElement
    veriyed title  after search

--- src/test/java/com/trycloud/tests/pages/Google_Home.java
+++ src/test/java/com/trycloud/tests/pages/Google_Home.java
@@ -2,6 +2,7 @@ package com.trycloud.tests.pages;
 
 import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -14,11 +15,17 @@ public class Google_Home {
     @FindBy(xpath = "//a[.='Gmail']")
     private WebElement gmailLink;
 
+    @FindBy(name = "q")
+    private WebElement googleSearchInput;
+
     public void clickGmailLink(){
         Driver.getDriver().get(ConfigurationReader.getProperty("urlGoogle"));
         gmailLink.click();
+    }
 
-
+    public void sendItemsToGoogleSearch(String items){
+        Driver.getDriver().get(ConfigurationReader.getProperty("urlGoogle"));
+        googleSearchInput.sendKeys(items + Keys.ENTER);
     }
 
 
