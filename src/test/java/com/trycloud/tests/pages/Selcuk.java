e1a41ff8b21060a2570423147993112ea6cd74f8
seljukqa <seljukqa@gmail.com>
2021-03-01 00:16:03 -0500

    demirci yazdim

--- src/test/java/com/trycloud/tests/pages/Selcuk.java
+++ src/test/java/com/trycloud/tests/pages/Selcuk.java
@@ -3,5 +3,6 @@ package com.trycloud.tests.pages;
 public class Selcuk {
     public static void main(String[] args) {
         System.out.println("selcuk....");
+        System.out.println("Demirci....");
     }
 }
