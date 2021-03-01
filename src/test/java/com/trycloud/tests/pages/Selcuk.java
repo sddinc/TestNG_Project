a59a9882f76793c3a9447f427a0f78bbf79a2294
seljukqa <seljukqa@gmail.com>
2021-03-01 00:50:37 -0500

    last update

--- src/test/java/com/trycloud/tests/pages/Selcuk.java
+++ src/test/java/com/trycloud/tests/pages/Selcuk.java
@@ -3,8 +3,13 @@ package com.trycloud.tests.pages;
 public class Selcuk {
     public static void main(String[] args) {
 
+
         System.out.println("Heydar");
         System.out.println("Jabbarov");
+
+        System.out.println("selcuk....");
+        System.out.println("Demirci");
+
 
     }
 }
